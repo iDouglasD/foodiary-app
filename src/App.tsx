6d9a19b99745a0e558ac7084d8c83fce01d92076
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-19 07:50:41 -0300

    feat(app): integrate DailyStats component and remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import {
   useFonts,
   HostGrotesk_400Regular,
@@ -15,6 +14,7 @@ import './styles/global.css';
 import { HomeHeader } from './components/home-header';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DateSwitcher } from './components/date-switcher';
+import { DailyStats } from './components/daily-stats';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -37,6 +37,15 @@ export default function App() {
       <SafeAreaProvider>
         <HomeHeader />
         <DateSwitcher />
+        <View className='mt-2'>
+          <DailyStats
+            calories={{ goal: 2000, current: 1350 }}
+            proteins={{ goal: 150, current: 90 }}
+            carbohydrates={{ goal: 250, current: 180 }}
+            fats={{ goal: 70, current: 50 }}
+          />
+        </View>
+        <View className='h-px mt-7 bg-gray-200' />
       </SafeAreaProvider>
     </View>
   );
