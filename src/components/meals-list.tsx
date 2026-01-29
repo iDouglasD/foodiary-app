63a5d7fd32ce03acf70fe5d2734109dab12d1380
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-01-29 08:35:27 -0300

    style: add bottom padding to meals list

--- src/components/meals-list.tsx
+++ src/components/meals-list.tsx
@@ -2,9 +2,11 @@ import { FlatList, Text, View } from "react-native";
 import { MealCard } from "./meal-card";
 import { MealsListHeader } from "./meals-list-header";
 import { Separetor } from "./separator";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 
 export function MealsList() {
+  const { bottom } = useSafeAreaInsets();
 
   const meals = [
     {
@@ -27,6 +29,7 @@ export function MealsList() {
       keyExtractor={meal => meal.id}
       ListHeaderComponent={<MealsListHeader />}
       ItemSeparatorComponent={Separetor}
+      contentContainerStyle={{ paddingBottom: 80 + bottom + 16 }}
       renderItem={({ item }) => (
         <View className="mx-5">
           <MealCard
